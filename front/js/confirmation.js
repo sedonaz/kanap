5c7aaf159d929ac0b99cd3e52f6354b804c2bf8d
sedonaz <pauline.michelon@gmail.com>
2022-12-29 17:08:37 +0100

    ajout de la page confirmation

---
+++ front/js/confirmation.js
@@ -0,0 +1,12 @@
+
+let id = new URL(window.location.href).searchParams.get("id");
+//On sélectionne l'élément HTML où s'affichera le numéro de commande 
+let orderId = document.querySelector('#orderId');
+//On insère le numéro de commande dans le code html
+orderId.innerHTML = id;
+alert('Votre commande a été validée ! Merci pour votre achat')
+//On néttoie le localStorage
+localStorage.clear();
+
+
+
