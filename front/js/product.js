7126d3dc110bf7820b734365ed30280593e934d1
sedonaz <pauline.michelon@gmail.com>
2022-12-29 18:00:45 +0100

    ajout commentaire + indentation

--- front/js/product.js
+++ front/js/product.js
@@ -1,54 +1,61 @@
+//AFFICHAGE DU PRODUIT CHOISI
+//On récupère les données de l'API du produit séléctionné
 let params = (new URL(document.location)).searchParams;
 let id = params.get ("id");
 async function getProductById(){
-await fetch(`http://localhost:3000/api/products/${id}`,{
+  await fetch(`http://localhost:3000/api/products/${id}`,{
   method: "GET", 
   cache: "default",
 })
-  .then (function(res){
-    if (res.ok ){
-      return res.json();
-    }
-  })
-  .then(function(data){
-    component = data;
-  })
+.then (function(res){
+  if (res.ok ){
+    return res.json();
+  }
+})
+.then(function(data){
+  component = data;
+})
 };
- async function showProduct(){
+//Affichage des détails du produit dans le code html
+async function showProduct(){
   await getProductById();
-  const divImage = document.querySelector("main article div.item__img");
+  let divImage = document.querySelector("main article div.item__img");
+  //On crée la balise img pour insérer l'image
   let image = document.createElement('img');
+  //On récupère les données pour les mettre au bon endroit dans le code HTML
   image.src = `${component.imageUrl}`;
   document.getElementById('title').innerText=`${component.name}`;
   document.getElementById('price').innerText=`${component.price}`;
   document.getElementById('description').innerText= `${component.description}`;
   divImage.append(image);
- }
- async function getColors(){
+}
+//Fonction pour choisir la couleur
+async function getColors(){
   await getProductById();
   let colors = component.colors;
   const select = document.querySelector("#colors");
   for ( let color in colors){
     const select = document.getElementById('colors');
-
-        let newColor = document.createElement('option');
-        newColor.value = colors[color];
-        newColor.innerHTML = colors[color];
-
-        select.append(newColor);
-
+    //On crée la balise option value pour le choix des couleurs
+    let newColor = document.createElement('option');
+    newColor.value = colors[color];
+    newColor.innerHTML = colors[color];
+    select.append(newColor);
+    
   }
- }
- showProduct();
- getColors();
-
-  const productQuantity = document.getElementById("quantity");
-  const colorChoice = document.getElementById("colors");
-  const btn_addToCart = document.querySelector("#addToCart");
-  let productTable = JSON.parse(localStorage.getItem("productTable"));
-  btn_addToCart.addEventListener("click", async function(e) {
-    await getProductById();
-    e.preventDefault();  
+}
+showProduct();
+getColors();
+//Variables pour la couleur, la quantité, le bouton et le panier
+let productQuantity = document.getElementById("quantity");
+let colorChoice = document.getElementById("colors");
+let btn_addToCart = document.querySelector("#addToCart");
+let productTable = JSON.parse(localStorage.getItem("cart"));
+//Ajout du listener sur le bouton ajouter au panier
+btn_addToCart.addEventListener("click", async function(e) {
+  await getProductById();
+  e.preventDefault();  
+  //Variable contenant les options choisies 
   let productOption = {
     id : component._id,
     color : colorChoice.value,
@@ -60,28 +67,33 @@ await fetch(`http://localhost:3000/api/products/${id}`,{
   if (colorChoice.value == 0 || productQuantity.value == 0){
     alert("la couleur ou la quantité n'est pas définie");
   }
+  //LOCALSTORAGE
+  //Si un produit est déja dans le panier avec la quantité et la couleur
   if (productTable){
-    productTable = productTable.filter(el => {
-      return el.id !== productOption.id || el.color !== productOption.color;
-    })
+    let productFound = productTable.find((p) => p.id === productOption.id && p.color === productOption.color);
+    if(productFound){
+      productFound.quantity = parseInt(productFound.quantity) + parseInt(productQuantity.value); 
+    }
+    else{
+      productTable.push(productOption);
+    }
+    //Enregistrement du panier dans le localStorage
+    localStorage.setItem("productTable", JSON.stringify(productTable));
   }
-  if (productTable){
-    productTable.push(productOption);
-    localStorage.setItem("productOption", JSON.stringify(productTable));
-  }
+  //Si le panier est vide 
   else{
     productTable = [];
     productTable.push(productOption);
-    localStorage.setItem("productOption", JSON.stringify(productTable));
+    localStorage.setItem("productTable", JSON.stringify(productTable));
     alert("le produit à été ajouté au panier");
   }
-
-  console.log(productTable);
-  })
-
-
-
   
-        
   
+})
+
+
+
+
+
+
 
